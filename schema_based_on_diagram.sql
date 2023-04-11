ee64b9111f000ce5ee466cf0f660f6aa35ee98ff
alicemirigo92 <alicemirigo92@gmai.com>
2023-04-11 15:23:13 +0300

    Made required changes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,6 +11,11 @@ CREATE TABLE medical_histories (
     status varchar(50) NOT NULL
 );
 
+CREATE TABLE medical_histories_has_treatments (
+    medical_history_id int references medical_histories(id),
+    treatment_id int references treatments(id),
+);
+
 CREATE TABLE invoices (
     id SERIAL PRIMARY KEY,
     total_amount DECIMAL NOT NULL,
@@ -36,12 +41,16 @@ CREATE TABLE invoice_items (
 
 ALTER TABLE invoice_items 
 ADD FOREIGN KEY (invoice_id) REFERENCES invoices (id);
+CREATE INDEX ON invoice_items (invoice_id);
 
 ALTER TABLE invoice_items 
 ADD FOREIGN KEY (treatment_id) REFERENCES treatments (id);
+CREATE INDEX ON invoice_items (treatment_id);
 
 ALTER TABLE invoices 
 ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id);
+CREATE INDEX ON invoices (medical_history_id);
 
 ALTER TABLE medical_histories 
 ADD FOREIGN KEY (patient_id) REFERENCES patients (id);
+CREATE INDEX ON medical_histories (patient_id);
